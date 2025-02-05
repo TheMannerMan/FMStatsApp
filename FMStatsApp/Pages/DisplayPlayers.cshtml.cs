93d44fb34d491818eadbef903d1c23b76f68964d
Henri Lehtonen <henri.lehtonen.87@gmail.com>
2025-02-05 11:13:51 +0100

    Selecting roles in DisplayPlayers working again. Next: Continue to break out Formation Selecter to an individual page.

--- FMStatsApp/Pages/DisplayPlayers.cshtml.cs
+++ FMStatsApp/Pages/DisplayPlayers.cshtml.cs
@@ -11,16 +11,16 @@ namespace FMStatsApp.Pages
 	{
 		//private readonly PlayerStorageService _service;
 
-		public List<SelectListItem> AvailableFormations { get; set; } = new();
+		//public List<SelectListItem> AvailableFormations { get; set; } = new();
 
-		[BindProperty]
-		public string SelectedFormation { get; set; }
+		//[BindProperty]
+		//public string SelectedFormation { get; set; }
 
-		public List<Position> AvailablePositions { get; set; } = Enum.GetValues<Position>().ToList();
+		//public List<Position> AvailablePositions { get; set; } = Enum.GetValues<Position>().ToList();
 
 
-		[BindProperty]
-		public Dictionary<Position, string> SelectedRoles { get; set; } = new();
+		//[BindProperty]
+		//public Dictionary<Position, string> SelectedRoles { get; set; } = new();
 
 		public List<string> ForwardRoles { get; set; } = RoleCatalog.AllRoles
 			.Where(r => r.GeneralPosition == GeneralPosition.Forward)
@@ -52,9 +52,9 @@ namespace FMStatsApp.Pages
 		{
 			Players = _httpContextAccessor.HttpContext.Session.GetObjectFromJson<List<Player>>("Players");
 
-			AvailableFormations = FormationCatalog.AllFormations
+			/*AvailableFormations = FormationCatalog.AllFormations
 				.Select(f => new SelectListItem { Value = f.Name, Text = f.Name })
-				.ToList();
+				.ToList(); */
 
 			return Page();
 		}
@@ -65,7 +65,7 @@ namespace FMStatsApp.Pages
 			return Page();
 		}
 
-		public IActionResult OnPostFormation()
+		/*public IActionResult OnPostFormation()
 		{
 			if (string.IsNullOrEmpty(SelectedFormation))
 			{
@@ -75,6 +75,6 @@ namespace FMStatsApp.Pages
 
 			// Här kan vi hantera vad som händer när en formation väljs
 			return RedirectToPage("NextPage"); // Ändra till rätt sida
-		}
+		} */
 	}
 }
