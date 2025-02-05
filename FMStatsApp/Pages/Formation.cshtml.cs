bc73c597343e17920722c1e1b19995b0d09923d5
Henri Lehtonen <henri.lehtonen.87@gmail.com>
2025-02-05 13:48:24 +0100

    I have started the build of a the page Formation. I noticed I have to rework the application so a Role can have many Positions. At the moment it can only have one. I will finish that before working on the page.

---
+++ FMStatsApp/Pages/Formation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMStatsApp.Pages
+{
+    public class FormationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- FMStatsApp/Pages/FormationSelecter.cshtml.cs
+++ FMStatsApp/Pages/FormationSelecter.cshtml.cs
@@ -2,28 +2,34 @@ using FMStatsApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Runtime.CompilerServices;
 
 namespace FMStatsApp.Pages
 {
     public class FormationSelecterModel : PageModel
     {
-		private readonly IHttpContextAccessor _httpContextAccessor;
-		public FormationSelecterModel(IHttpContextAccessor httpContextAccessor)
+		//private readonly IHttpContextAccessor _httpContextAccessor;
+		public SelectList AvailableFormations { get; set; }
+		public Formation SelectedFormation { get; set; } = null;
+
+		[BindProperty]
+		public string SelectedFormationName { get; set; }
+
+		/*public FormationSelecterModel(IHttpContextAccessor httpContextAccessor)
 		{
 			_httpContextAccessor = httpContextAccessor;
-		}
-
-
-
-		public List<SelectListItem> AvailableFormations { get; set; } = new();
-		[BindProperty]
-		public string SelectedFormation { get; set; }
+			
+		} */
 
 		//public List<Player> Players { get; set; }
 
 		public List<Position> AvailablePositions { get; set; } = Enum.GetValues<Position>().ToList();
 
-
+		private SelectList GetFormations()
+		{
+			List<Formation> formations = FormationCatalog.AllFormations;
+			return new SelectList(formations, nameof(Formation.Name), nameof(Formation.Name));
+		}
 
 
 
@@ -32,8 +38,16 @@ namespace FMStatsApp.Pages
 			/*AvailableFormations = FormationCatalog.AllFormations
 				.Select(f => new SelectListItem { Value = f.Name, Text = f.Name })
 				.ToList(); */
+			AvailableFormations = GetFormations();
 
 			return Page();
 		}
-    }
+
+		public IActionResult OnPost()
+		{
+			SelectedFormation = FormationCatalog.AllFormations.Find(f => f.Name == SelectedFormationName);
+			return RedirectToPage("Formation", SelectedFormation);
+		}
+
+	}
 }
